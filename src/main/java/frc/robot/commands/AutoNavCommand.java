a31c21d43e5386900da69ac98e7eb526ed5cd798
programming353 <58999047+programming353@users.noreply.github.com>
2021-02-04 22:11:55 -0500

    Changes as of 2/4/2021

--- src/main/java/frc/robot/commands/AutoNavCommand.java
+++ src/main/java/frc/robot/commands/AutoNavCommand.java
@@ -1,13 +1,15 @@
 package frc.robot.commands;
-
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveSubsystem;
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.GyroBase;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 
 public class AutoNavCommand extends CommandBase{
 
   private final DriveSubsystem driveSubsystem;
+  Gyro gyro = new ADXRS450_Gyro(SPI.Port.kMXP);
 
   public AutoNavCommand(DriveSubsystem subsystem) {
     // Use addRequirements() here to declare subsystem dependencies.
@@ -16,6 +18,6 @@ public class AutoNavCommand extends CommandBase{
   }
 
   public void slalom(){
-    
+
   }
 }
