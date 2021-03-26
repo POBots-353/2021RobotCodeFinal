6a12fdc56a31501c5d062bd25bd6a272431407c4
programming353 <programming353@gmail.com>
2021-03-26 12:56:44 -0400

    Update GalacticSearchSubsystem.java

--- src/main/java/frc/robot/subsystems/GalacticSearchSubsystem.java
+++ src/main/java/frc/robot/subsystems/GalacticSearchSubsystem.java
@@ -62,7 +62,7 @@ public class GalacticSearchSubsystem extends SubsystemBase {
   
   // The gyro sensor
   //Fix this
-  Gyro m_gyro = new ADXRS450_Gyro(/*SPI.Port.kMXP*/);
+  Gyro m_gyro = new ADXRS450_Gyro(SPI.Port.kOnboardCS1);
 
   // Odometry class for tracking robot pose
   private final DifferentialDriveOdometry m_odometry;
